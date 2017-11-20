d9815cc26aef60fe2272f95bfc1737237576632e
pablopalillo <pablo.576@hotmail.com>
2017-11-19 21:51:00 -0500

    adicion de cordenadas x y y

--- pong/app.js
+++ pong/app.js
@@ -3,15 +3,21 @@ var canvasContext     = null;
 var framesForSeconds  = null;
 var ballX             = null;
 var ballY             = null;
-var ballSpeed         = null;
+var ballSpeedX        = null;
+var ballSpeedY        = null;
+var player1Pos        = null;
+var player2Pos        = null;
 
 window.onload = function() {
   canvas            = document.getElementById('game-board');
   canvasContext     = canvas.getContext("2d");
   framesForSeconds  = 30;
   ballX             = 50;
-  ballY             = 50;
-  ballSpeed         = 5;
+  ballY             = 150;
+  ballSpeedX        = 10;
+  ballSpeedY        = 4;
+  player1Pos        = 300;
+  player2Pos        = 300;
 
   setInterval(function(){
       draw();
@@ -24,33 +30,37 @@ window.onload = function() {
 function draw()
 {
   // board
-  canvasContext.fillStyle = "black";
-  canvasContext.fillRect(0, 0, canvas.width, canvas.height );
+  drawRect(0, 0, canvas.width, canvas.height, "black");
 
   //player1
-  canvasContext.fillStyle = "white";
-  canvasContext.fillRect(0, 350, 15, 100);
+  drawRect(0, player1Pos, 15, 100, "white");
 
   //player2
-  canvasContext.fillStyle = "white";
-  canvasContext.fillRect(canvas.width - 15, 350, 15, 100);
+  drawRect(canvas.width - 15, player2Pos, 15, 100, "white");
 
-  // ball
-  canvasContext.fillStyle = "white";
-  canvasContext.fillRect(ballX, ballY, 10, 10);
-
-
+  //ball
+  drawCircle(ballX, ballY, 10, "white");
 
 }
 
 function move()
 {
-  ballX = ballX + ballSpeed;
-  if (ballX === canvas.width) {
-    ballSpeed = -5;
+  ballX = ballX + ballSpeedX;
+  ballY = ballY + ballSpeedY;
+
+  if (ballX > canvas.width) {
+    ballSpeedX = -ballSpeedX;
   } else {
-    if(ballX === 0) {
-      ballSpeed = 5;
+    if(ballX <= 0) {
+      ballSpeedX = -ballSpeedX;
+    }
+  }
+
+  if (ballY > canvas.height) {
+    ballSpeedY = -ballSpeedY;
+  } else {
+    if(ballY < 0) {
+      ballSpeedY = -ballSpeedY;
     }
   }
 }
@@ -58,4 +68,23 @@ function move()
 function getInterval(frames)
 {
   return 1000 / frames;
+}
+
+function drawRect(x, y, width, height, color)
+{
+  canvasContext.fillStyle = color;
+  canvasContext.fillRect(x, y, width, height);
+}
+
+function drawCircle(x, y, radius, color )
+{
+    /** ball **/
+    canvasContext.beginPath();
+    canvasContext.fillStyle = color;
+    canvasContext.arc(x, y , radius, 0, Math.PI*2, true );
+    canvasContext.fill()
+
+    // point
+    //canvasContext.fillStyle = "white";
+    //canvasContext.fillRect(ballX, ballY, 10, 10);
 }
